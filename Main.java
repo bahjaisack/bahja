b2133c35d5e12978cf0562571f3cef9e0eb661f2
Bahja <bahjaisack@gmail.com>
2022-12-19 19:23:54 +0300

    DSA with java

---
+++ Main.java
@@ -0,0 +1,54 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    int[] nums = {23, 45, 1, 2, 8, 19, -3, 16, -11, 28};
+	    int target = 19;
+	    boolean ans = linearSearch3(nums, target);
+        System.out.println(ans);
+    }
+
+    // search the target and return true or false
+    static boolean linearSearch3(int[] arr, int target) {
+        if (arr.length == 0) {
+            return false;
+        }
+
+        // run a for loop
+        for (int element : arr) {
+            if (element == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static int linearSearch2(int[] arr, int target) {
+        if (arr.length == 0) {
+            return -1;
+        }
+
+        for (int element : arr) {
+            if (element == target) {
+                return element;
+            }
+        }
+     
+        return Integer.MAX_VALUE;
+    }
+
+    static int linearSearch(int[] arr, int target) {
+        if (arr.length == 0) {
+            return -1;
+        }
+
+        for (int index = 0; index < arr.length; index++) {
+  
+            int element = arr[index];
+            if (element == target) {
+                return index;
+            }
+        }
+        return -1;
+    }
+
+}
